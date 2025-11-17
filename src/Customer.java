e7147fdbeaa52b41dff6074f613189c4cb869ddb
JanaAbuSubha <Jana121205@gmail.com>
2025-11-17 15:20:13 -0600

    planning

---
+++ src/Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    int satisfaction;
+    int willingnessToTip;
+    int pickiness; 
+
+    public Customer(){
+        
+    }
+
+
+}
---
+++ src/TacoMain.java
@@ -0,0 +1,16 @@
+public class TacoMain {
+    HashMap<String, Customer>;
+    int tips; 
+
+    public static void main(String[] args) {
+        // run intro text
+        // prompt user to press start
+        // once begins, enter c-style loop of 10 times
+            //  get a random customer, add them to queue
+            // call tree dialogue method ****** (figure this out, do we need multiple trees?) 
+            // calculate amount of money earned from interaction 
+            // append to tips instance variable 
+        // 
+
+    }
+}
