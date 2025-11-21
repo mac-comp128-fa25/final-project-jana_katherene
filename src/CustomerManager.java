ada1634865c6c2624f42fa661488f4ecf00f55f9
JanaAbuSubha <Jana121205@gmail.com>
2025-11-21 15:17:42 -0600

    Update CustomerManager

--- src/CustomerManager.java
+++ src/CustomerManager.java
@@ -1,18 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 public class CustomerManager{
-    private HashMap<String, Customer> allCharacters;
-    
+    private Map<String, Customer> allCharacters;
+    Random random = new Random(); 
+
     public void constructCustomerMap(){
-        allCharacters.put("sample1", new Customer())
+        allCharacters.put("sample1", new Customer(99, 50));
+        allCharacters.put("sample2", new Customer(0, 99));
     }
     
-    public HashMap<String, Customer> getAllCharacters(){
+    public Map<String, Customer> getAllCustomers(){
         return allCharacters;
     }
 
     public Customer getrandomCustomer(){
-        return 
+        Collection<Customer> values = allCharacters.values();
+        List<Customer> valueList = new ArrayList<>(values);
+        Integer randomNum = random.nextInt(valueList.size());
+        Customer randomcustomer = valueList.get(randomNum);
+        return randomcustomer;
     }
 }
