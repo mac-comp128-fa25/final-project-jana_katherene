e6d0d5454d1322e40be9813839432e2ed1bd515f
JanaAbuSubha <Jana121205@gmail.com>
2025-11-21 15:00:47 -0600

    update

--- src/CustomerManager.java
+++ src/CustomerManager.java
@@ -1,3 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CustomerManager{
+    private HashMap<String, Customer> allCharacters;
     
+    public void constructCustomerMap(){
+        allCharacters.put("sample1", new Customer())
+    }
+    
+    public HashMap<String, Customer> getAllCharacters(){
+        return allCharacters;
+    }
+
+    public Customer getrandomCustomer(){
+        return 
+    }
 }
