5cb50febadb23e5d02f7371f3d32aadebf81ba39
JanaAbuSubha <Jana121205@gmail.com>
2025-12-01 19:46:08 -0600

    Playing with main

--- src/TacoMain.java
+++ src/TacoMain.java
@@ -1,19 +1,21 @@
 import java.util.HashMap;
+import java.util.Queue;
 import java.util.Scanner;
 public class TacoMain {
     HashMap<String, Customer> mapOne;
     static Customer sally;
     int tips; 
+    String username;
 
     public static void main(String[] args) {
         //sample text interaction
-        sally = new Customer(0, 0, "sally");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name:");
+        // sally = new Customer(0, 0, "sally");
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Enter your name:");
 
-        sally.name = scanner.nextLine();
+        // sally.name = scanner.nextLine();
 
-        System.out.println(sally.getName());
+        // System.out.println(sally.getName());
         // run intro text
         // prompt user to press start
         // once begins, enter c-style loop of 10 times
@@ -23,5 +25,12 @@ public class TacoMain {
             // append to tips instance variable 
         // 
 
+        System.out.println("Hello! My name is Your Boss. Welcome to your first day at the Taco Shop. What's your name?");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        
+        System.out.println("That's an okay name. Welcome " + username + "! ");
+        System.out.println("Today we expect 10 customers. You're new here, so try to be courteous to our town's people. They can be a handful.... Good luck!");
+        
     }
 }
